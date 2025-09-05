9823619bb87244ac6b27bf63988b14cf3e3246cb
web-coder-of-rpi <leo.jarnicki@gmail.com>
2025-09-05 18:19:01 +0800

    set up website

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @app.route("/")
 def index():
-  return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
